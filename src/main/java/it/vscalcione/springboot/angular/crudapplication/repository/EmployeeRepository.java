ca9d7e5058deddebd7892491cff4320abfecf729
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-16 11:04:47 +0200

    Update

--- src/main/java/it/vscalcione/springboot/angular/crudapplication/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package it.vscalcione.springboot.angular.crudapplication.repository;
-
-import it.vscalcione.springboot.angular.crudapplication.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
