48dca2c9ad03234256dc3de6bca09ccb16f78127
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-16 17:48:30 +0200

    Fixing Cors call on http://localhost:4200

--- angular9-springboot-client/src/app/components/employee-list/employee-list.component.ts
+++ angular9-springboot-client/src/app/components/employee-list/employee-list.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
-  templateUrl: "./employee-list.component.html",
+  templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
@@ -19,15 +19,15 @@ export class EmployeeListComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void{
     this.reloadData();
   }
 
-  reloadData() {
+  reloadData(): void{
     this.employees = this.employeeService.getEmployeesList();
   }
 
-  deleteEmployee(id: number) {
+  deleteEmployee(id: number): void{
     this.employeeService.deleteEmployee(id).subscribe(
       data => {
         console.log(data);
@@ -37,7 +37,7 @@ export class EmployeeListComponent implements OnInit {
     );
   }
 
-  employeeDetails(id: number) {
+  employeeDetails(id: number): void {
     this.router.navigate(['details', id]);
   }
 }
